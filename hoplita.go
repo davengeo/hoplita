673b78e53dcbc34e124d34032c6f059efd65a54d
David Navarro Alvarez <davengeo@yahoo.es>
2016-07-10 18:48:46 +0200

    with key and document type

--- hoplita.go
+++ hoplita.go
@@ -9,10 +9,10 @@ import (
 func main() {
 	router := gin.Default()
 	router.GET("/ping", func(c *gin.Context) {
-		firstname := c.DefaultQuery("firstname", "Guest")
-		lastname := c.Query("lastname") // shortcut for c.Request.URL.Query().Get("lastname")
+		key := c.DefaultQuery("key", "none")
+		documentType := c.Query("document-type")
 
-		c.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+		c.String(http.StatusOK, "Looking for %s %s", key, documentType)
 	})
 	router.Run(":8081") // listen and server on 0.0.0.0:8080
 }
