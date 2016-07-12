5cc1062a888332f5ef072b5e2ef95f1e2c982958
David Navarro Alvarez <davengeo@yahoo.es>
2016-07-12 14:07:46 +0200

    working with channels

--- hoplita.go
+++ hoplita.go
@@ -8,7 +8,7 @@ import (
 )
 
 type message struct {
-
+	key string
 	value interface {}
 
 }
@@ -17,26 +17,37 @@ func main() {
 	router := gin.Default()
 
 	msg := message{}
+	//var lock sync.Mutex
+
+	income := make(chan message)
 
 	router.GET("/webhook", func(c *gin.Context) {
 		key := c.DefaultQuery("key", "none")
 		documentType := c.Query("document-type")
 		c.String(http.StatusOK, "Looking for %s %s", key, documentType)
+		//lock.Lock()
 		msg.value = key
+		income<-msg
+		//lock.Unlock()
 	})
 
-	go OrdersLoop(&msg)
+	go OrdersLoop(income)
 
 	router.Run(":8081")
 
 }
 
 
-func OrdersLoop(messages *message) {
+func OrdersLoop(income chan message) {
 
+	messages := message{}
 	for {
 		time.Sleep(300 * time.Millisecond)
-		log.Printf("hello:%s", messages.value)
+		if messages.value != nil {
+			log.Printf("hello:%s", messages.value)
+		}
+		messages=<-income
 	}
 
 }
+
